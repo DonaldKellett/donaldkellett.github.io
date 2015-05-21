b5681fa51a4dcaa3dc676b4b6da95722f4c7fce2
DonaldKellett <i.donaldl@me.com>
2015-05-21 22:12:10 +0800

    Added countdown timer

---
+++ donald/assets/js/404.js
@@ -0,0 +1,20 @@
+/*
+	404.js
+	The countdown redirect JS file for the 404 page
+	(c) Donald Leung.  All rights reserved.
+*/
+
+var seconds = 60;
+
+function timedRedirect() {
+	if (seconds > 0) {
+		document.getElementById("redirect").innerHTML = "If you do nothing, you will be redirected to <strong>Donald Leung - Home</strong> in " + seconds + " seconds.";
+	} else {
+		document.getElementById("redirect").innerHTML = "You will now be redirected to <strong>Donald Leung - Home</strong>."
+		// window.location = "http://donaldkellett.github.io";
+	}
+	seconds--;
+	setTimeout("timedRedirect()", 1000);
+}
+
+timedRedirect();
